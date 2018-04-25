026e8ac92b5629a10a35cbd8e6fee97496c16cef
snoweee322 <diman199909@gmail.com>
2018-04-25 22:52:22 +0700

    Add game() and leaderboard() procedures

--- source/main.c
+++ source/main.c
@@ -1,7 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void game();
+void leaderboard();
+
 int main()
 {
 	char c;
-	while (1)
+	while (1) // switch-меню
 	{
     	puts(" 1. Start game");
     	puts(" 2. Leaderboard");
@@ -9,11 +16,28 @@ int main()
     	c = getch();
     	switch(c)
     	{
-			case '1': game(); break;
-			case '2': leaderboard(); break;
-			case '3': return 0;
+			case '1': game(); break; // вызов game()
+			case '2': leaderboard(); break; // вызов leaderboard()
+			case '3': return 0; // выход
 			default : puts("Wrong key");
 		}
 	}
 	return 0;
+}
+
+void game()
+{
+    FILE *st;
+    int lines = 0;
+    char word_buffer[25]; // буфер для хранения строки из файла
+    st = fopen("dictionary.txt", "r"); // чтение файла
+    while (fscanf(st, "%s", &word_buffer) != EOF) // вычисление количества строк в файле
+	{
+        lines++;
+    }
+    printf("%d",lines);
+} 
+
+void leaderboard()
+{
 }
