45980f0224b9047ffd02464b2d30b79ecd520246
snoweee322 <diman199909@gmail.com>
2018-04-29 19:50:56 +0700

    Finish development of basic functionality

--- source/main.c
+++ source/main.c
@@ -1,22 +1,29 @@
 ﻿#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 void game();
 void leaderboard();
 
+void hangman(int n) //виселица
+{
+}
+
 int main()
 {
+    srand(time(NULL));
 	char c;
 	while (1) // switch-меню
 	{
+	    system("cls");
     	puts(" 1. Start game");
     	puts(" 2. Leaderboard");
     	puts(" 3. Exit");
-    	c = getch();
+    	c = getchar();
     	switch(c)
     	{
-			case '1': game(); break; // вызов game()
+            case '1': game(); break; // вызов game()
 			case '2': leaderboard(); break; // вызов leaderboard()
 			case '3': return 0; // выход
 			default : puts("Wrong key");
@@ -39,13 +46,79 @@ void game()
     random_line = rand() % lines; // выбор слова (выбор случайной строки)
     rewind(st); // перемещение указателя в начало файла
     int i;
-    for (i = 0; i < random_line; i++) 
+    for (i = 0; i < random_line; i++)
 	{
 		fscanf(st, "%s", word_buffer); // считывание слова, которое будет загадано
 	}
 	fclose(st);
-	int world_size = strlen(word_buffer);
-	printf("%d", world_size);
+	int word_size = strlen(word_buffer); // кол-во символов в слове
+	char *hidden_word = (char*)malloc(word_size); // дин.массив с отгаданными буквами
+	for (i = 0; i < word_size; i++) // заполнение массива прочерками (неизветные буквы)
+    {
+        hidden_word[i] = '_';
+        hidden_word[i+1] = '\0';
+    }
+    int guessed_letters = word_size; // кол-во неотгаданных букв (счётчик)
+    int wrong_letters = 0; // неправильно отгаданные буквы (до 9)
+    char letter_array[50] = {NULL}; // массив, куда будут вводиться отгадываемые буквы
+    int letter_count = 0;
+    int flag = 0;
+    while (guessed_letters >= 0 && wrong_letters <= 9)
+    {
+        if (flag == 1) // неотгаданная буква
+        {
+            wrong_letters++;
+        }
+        flag = 1;
+        system("cls");
+        printf("A WORD: >> %s <<\n", word_buffer); // ОТЛАДКА
+        printf("GUESSED LETTERS: >> %d <<\n", guessed_letters); // ОТЛАДКА
+        printf("WRONG LETTERS: >> %d <<\n", wrong_letters); // ОТЛАДКА
+        hangman(wrong_letters);
+        if (guessed_letters == 0) // условие победы
+        {
+            printf("%s", word_buffer);
+            printf("\nYou won!\nPrint any key to countinue.\n\n");
+            free(hidden_word);
+            scanf(" %c");
+            return; // выход в меню
+        }
+        if (wrong_letters == 9) // условие поражения
+        {
+            printf("%s", word_buffer);
+            printf("\nYou lost!\nPrint any key to countinue.\n\n");
+            free(hidden_word);
+            scanf(" %c");
+            return; // выход в меню
+        }
+        printf("%s", hidden_word); // скрытое слово
+        printf("\nEntered letters:\n");
+        printf("%s", letter_array); // введенные буквы
+        printf("\nPrint a letter: ");
+        char letter = -1;
+        scanf(" %c", &letter);
+        for (i = 0; i < word_size; i++) // проверка введеных букв
+        {
+            if (word_buffer[i] == letter)
+            {
+                if (!(strchr(hidden_word + i, letter))) // если буква не отгадана
+                {
+                    flag = 0;
+                    guessed_letters--;
+                    hidden_word[i] = word_buffer[i];
+                }
+            } else if (strchr(letter_array, letter) || letter <= 96 || letter >= 123) // повторный ввод или неправильный ввод (a = 97 z = 122 по ascii)
+            {
+                flag = 0;
+            }
+        }
+        if (!(strchr(letter_array, letter) || letter <= 96 || letter >= 123)) // запись ПРАВИЛЬНО введённых букв
+        {
+            letter_array[letter_count++] = letter;
+        }
+    }
+    free(hidden_word);
+    return;
 }
 
 void leaderboard()
